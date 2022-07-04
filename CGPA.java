68ed31ee0d2a948e7bc48d34f26383ae05c01bc9
churchil15 <churchil.rajpal15@gmail.com>
2022-07-04 20:53:44 +0530

    added CGPA.java

---
+++ CGPA.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import java.util.Scanner;
+public class CGPA {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        float TMO, TM, Percent, CGPA;
+//        System.out.println("***********This is a CGPA Calculator***********");
+//        System.out.print("Enter total marks of a subject (Example: 100 or 80): ");
+//        float a = sc.nextFloat();
+//        TM = a*3;
+//
+//        System.out.print("Enter marks of first subject: ");
+//        float m1 = sc.nextFloat();
+//
+//        System.out.print("Enter marks of second subject: ");
+//        float m2 = sc.nextFloat();
+//
+//        System.out.print("Enter marks of third subject: ");
+//        float m3 = sc.nextFloat();
+//
+//        TMO = m1 + m2 + m3;
+//        Percent = (TMO/TM)*100;
+//        System.out.println("Total marks obtained by student is: " + TMO);
+//        System.out.println("Percentage is: " + Percent + "%");
+//
+//        CGPA = Percent/9.5f;
+//        System.out.print("CGPA is: " + CGPA);
+
+//        System.out.println("What is your name: ");
+//        String str = sc.nextLine();
+//        System.out.print("Hello ", + str + ", have a good day!!");
+//        System.out.print("Hello ");
+//        System.out.print(str);
+//        System.out.println(", have a good day!!");
+
+//        System.out.println("Enter distance in kilometers");
+//        float km = sc.nextFloat();
+//        float miles;
+//        miles = km*0.621f;
+//        System.out.print("Distance in miles is: " + miles);
+
+        System.out.println("Enter your input: ");
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+
+//        System.out.println("Enter two floating point numbers for multiplication");
+//        float a = sc.nextFloat();
+//        float b = sc.nextFloat();
+//        float c;
+//        c = a*b;
+//        System.out.print("Multiplication is: " + c);
+
+//        int a = 10, b = 20, c;
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//
+//        c = a;
+//        a = b;
+//        b = c;
+
+//        a = a-b;
+//        b = a+b;
+//        a = b-a;
+////
+//        System.out.println("\na = " + a);
+//        System.out.println("b = " + b);
+
+//        if (a%2 == 0)
+//            System.out.println("a is even!!");
+//        else
+//            System.out.println("a is odd!!");
+
+//        System.out.println("Enter three numbers: ");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        if(a>b)
+//            if(a>c)
+//                System.out.println(a + " is largest");
+//            else
+//                System.out.println(c + " is largest");
+//        else
+//            if(b>c)
+//                System.out.println(b + " is largest");
+//            else
+//                System.out.println(c + " is largest");
+
+    }
+}
